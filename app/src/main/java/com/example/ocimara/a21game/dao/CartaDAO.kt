31629cf9a34d18b045ef1747fb2cf4fdd3171858
Ocimara <ocimarafbarcellos@gmail.com>
2018-02-05 22:00:19 -0200

    fazendo calculo
    lição de casa

---
+++ app/src/main/java/com/example/ocimara/a21game/dao/CartaDAO.kt
@@ -0,0 +1,29 @@
+package com.example.ocimara.a21game.dao
+
+/**
+ * Created by Kleber on 05/02/2018.
+ */
+import com.example.ocimara.a21game.R
+import com.example.ocimara.a21game.model.Carta
+
+class CartaDAO{
+    private val cartas = ListOf<Carta>(
+            Carta(R.drawable.as_de_espada, 1)
+                    Carta(R.drawable.as_de_espada,2)
+            Carta(R.drawable.as_de_espada, 3)
+            Carta(R.drawable.as_de_espada, 4)
+            Carta(R.drawable.as_de_espada, 5)
+            Carta(R.drawable.as_de_espada, 6)
+            Carta(R.drawable.as_de_espada, 7)
+            Carta(R.drawable.as_de_espada, 8)
+            Carta(R.drawable.as_de_espada, 9)
+            Carta(R.drawable.as_de_espada, 10)
+            Carta(R.drawable.as_de_espada, 11)
+
+
+
+    )
+
+
+
+}
---
+++ app/src/main/java/com/example/ocimara/a21game/model/Carta.kt
@@ -0,0 +1,6 @@
+package com.example.ocimara.a21game.model
+
+/**
+ * Created by Kleber on 05/02/2018.
+ */
+data class Carta(var resourceId: Int, var pontuacao: Int)
